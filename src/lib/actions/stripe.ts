331d5df968d2eda4ccbb02f92ffa222b6313b190
Kiran Kumar K <kirankumarwaste1@gmail.com>
2025-03-05 00:16:39 +0530

    localhost direction prod fix

--- src/lib/actions/stripe.ts
+++ src/lib/actions/stripe.ts
@@ -7,8 +7,8 @@ const stripe = new Stripe(process.env.STRIPE_SK_KEY!)
 export async function checkout(email: string, redirectTo: string) {
 
     return JSON.stringify(await stripe.checkout.sessions.create({
-        success_url: redirectTo || process.env.NEXT_PUBLIC_SITE_URL,
-        cancel_url: process.env.NEXT_PUBLIC_SITE_URL,
+        success_url: redirectTo || process.env.NEXT_PUBLIC_BASE_URL,
+        cancel_url: process.env.NEXT_PUBLIC_BASE_URL,
         customer_email: email,
         line_items: [{ price: process.env.PRO_PRICE_ID_MONTHLY, quantity: 1 }],
         mode: 'subscription'
