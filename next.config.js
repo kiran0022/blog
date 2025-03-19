c666028da2c33c96a61f03ab59e40c36c1685ece
Kiran Kumar K <kirankumarwaste1@gmail.com>
2025-03-19 13:47:00 +0530

    add: more image domain support

--- next.config.js
+++ next.config.js
@@ -9,6 +9,32 @@ const nextConfig = {
       {
         hostname: "images.unsplash.com",
         protocol: "https",
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.picjumbo.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.stocksnap.io',
+        pathname: '/**',
       },
     ],
   },
--- src/app/dashboard/schema/index.ts
+++ src/app/dashboard/schema/index.ts
@@ -1,6 +1,15 @@
 
 import * as z from "zod";
 
+const allowedImageHostnames = [
+  'images.unsplash.com',
+  'images.pexels.com',
+  'cdn.pixabay.com',
+  'img.freepik.com',
+  'cdn.picjumbo.com',
+  'cdn.stocksnap.io',
+];
+
 export const BlogFormSchema = z
   .object({
     title: z.string().min(5, {
@@ -13,21 +22,34 @@ export const BlogFormSchema = z
     isPremium: z.boolean(),
     isPublish: z.boolean(),
   })
-  .refine(
-    (data) => {
-      const image_url = data.image_url;
-      try {
-        const url = new URL(image_url);
+  // .refine(
+  //   (data) => {
+  //     
+  //     try {
+  //       const url = new URL(image_url);
 
-        return url.hostname === "images.unsplash.com";
-      } catch (error) {
-        return false;
-      }
-    },
-    {
-      message: "Currently we are only supporting the image from unsplash",
-      path: ["image_url"],
-    },
-  );
+  //       return url.hostname === "images.unsplash.com";
+  //     } catch (error) {
+  //       return false;
+  //     }
+  //   },
+  //   {
+  //     message: "Currently we are only supporting the image from unsplash",
+  //     path: ["image_url"],
+  //   },
+  // );
+
+  .refine((data) => {
+    const image_url = data.image_url;
+    try {
+      const { hostname } = new URL(image_url);
+      return allowedImageHostnames.includes(hostname);
+    } catch {
+      return false;
+    }
+  }, {
+    message: 'Currently, we only support images from specified sources.',
+    path: ['image_url'],
+  })
 
 export type BlogFormSchemaType = z.infer<typeof BlogFormSchema>;
